d211290c0e0c219fd304aed11bde7f46efb52d39
Denis Onder <denis_onder@protonmail.com>
2022-07-20 13:53:43 +0200

    chore: got rid of the name warning

--- src/global.rs
+++ src/global.rs
@@ -1,4 +1,4 @@
-pub mod CONSTANTS {
+pub mod constants {
     pub const TITLE: &str = "Magnus";
     pub const WIDTH: u32 = 800;
     pub const HEIGHT: u32 = 600;
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ mod actions;
 mod global;
 mod window;
 
-use global::CONSTANTS;
+use global::constants;
 
 fn main() {
     let options = window::WindowRenderOptions {
-        title: String::from(CONSTANTS::TITLE),
-        width: CONSTANTS::WIDTH,
-        height: CONSTANTS::HEIGHT,
+        title: String::from(constants::TITLE),
+        width: constants::WIDTH,
+        height: constants::HEIGHT,
         event_handler_func: actions::handle_event,
     };
 
