e0762950aca6bf074a812c3f729a466858ff5798
Denis Onder <denis_onder@protonmail.com>
2022-07-20 12:42:25 +0200

    feat: global state module

---
+++ src/global.rs
@@ -0,0 +1,5 @@
+pub mod CONSTANTS {
+    pub const TITLE: &str = "Magnus";
+    pub const WIDTH: u32 = 800;
+    pub const HEIGHT: u32 = 600;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
+mod global;
 mod window;
 
-mod CONSTANTS {
-    pub const TITLE: &str = "Magnus";
-    pub const WIDTH: u32 = 800;
-    pub const HEIGHT: u32 = 600;
-}
+use global::CONSTANTS;
 
 fn main() {
     window::render_window(CONSTANTS::TITLE, CONSTANTS::WIDTH, CONSTANTS::HEIGHT);
