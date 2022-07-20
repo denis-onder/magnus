c4f312dae55fc8e6452b883df8a5f61dd314d9a9
Denis Onder <denis_onder@protonmail.com>
2022-07-20 13:50:27 +0200

    feat: introducing the rand crate feat: added color debug command - F10: fills the window with a random color

--- src/actions.rs
+++ src/actions.rs
@@ -1,8 +1,25 @@
+extern crate rand;
 extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use sdl2::render::WindowCanvas;
+
+use rand::{thread_rng, Rng};
+
+fn handle_color_debug(canvas: &mut WindowCanvas) {
+    let mut rng = thread_rng();
+
+    let red: u8 = rng.gen_range(0..255);
+    let green: u8 = rng.gen_range(0..255);
+    let blue: u8 = rng.gen_range(0..255);
+
+    let rgb = Color::RGB(red, green, blue);
+    println!("{:?}", rgb);
+
+    canvas.set_draw_color(rgb);
+}
 
 pub fn handle_event(event: &Event, canvas: &mut WindowCanvas) {
     match event {
@@ -12,9 +29,9 @@ pub fn handle_event(event: &Event, canvas: &mut WindowCanvas) {
             ..
         } => std::process::exit(0),
         Event::KeyDown {
-            keycode: Some(Keycode::Space),
+            keycode: Some(Keycode::F10),
             ..
-        } => canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 0, 0)),
+        } => handle_color_debug(canvas),
         _ => {}
     }
 }
